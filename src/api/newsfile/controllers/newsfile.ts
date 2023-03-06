2662dccde99a3a380e2ea3fb3931a6c2272575ab
Lem Canady <lem.canady@gmail.com>
2023-03-05 19:24:55 -0800

    initial commit

---
+++ src/api/newsfile/controllers/newsfile.ts
@@ -0,0 +1,7 @@
+/**
+ * newsfile controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::newsfile.newsfile');
---
+++ src/api/newsfile/routes/newsfile.ts
@@ -0,0 +1,7 @@
+/**
+ * newsfile router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::newsfile.newsfile');
